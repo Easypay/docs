f362c02690f74f4a9ce68d9aee8ce9fa20c9b466
Miguel Branco <mig.jcb@gmail.com>
2022-11-09 11:19:41 +0000

    [CR-2090] Clarify Checkout testing option (#35)

---
+++ components/global/partial.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt-content :document="content" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true,
+    }
+  },
+  data: () => ({
+    content: []
+  }),
+  async fetch() {
+    this.content = await this.$content(this.path).fetch()
+  }
+}
+</script>
